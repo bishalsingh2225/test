7ff8f5249368206456b5cbd3c89e84b85ea26337
bishalsingh2225 <61987973+bishalsingh2225@users.noreply.github.com>
2020-09-02 18:35:33 +0530

    i changed your repo

--- hellow.c
+++ hellow.c
@@ -5,7 +5,7 @@ int main() {
    printf("my name is bishal kumar singh");
    printf("i forked this repo and now will send you a pull req");
    return 0;
-
+   my bishal is good
 ab mere kok kuch smz ni aa rha 
    le doobara change kia maine
 
